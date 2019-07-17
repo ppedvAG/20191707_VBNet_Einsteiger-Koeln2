726a7345cad758eba83e82787c88c6153c3bf3ca
JoeVice <DanielWill@users.noreply.github.com>
2019-07-17 11:41:57 +0200

    Nützliche Links

---
+++ HalloWelt/ZweitesProjekt/Module1.vb
@@ -0,0 +1,13 @@
+﻿Imports System.IO
+
+Module Module1
+
+    Sub Main()
+        File.AppendAllText()
+    End Sub
+
+    Sub MeineFunktion()
+        ' diese Funktion tut irgendwas
+    End Sub
+
+End Module
