60a9045432375b19c3dc8b943cb27e2ed2d6bf35
JoeVice <DanielWill@users.noreply.github.com>
2019-07-18 15:21:11 +0200

    Kontroll-Strukturen / Overload

--- KontrollStrukturen/KontrollStrukturen/Module1.vb
+++ KontrollStrukturen/KontrollStrukturen/Module1.vb
@@ -1,6 +1,7 @@
 ﻿Module Module1
 
     Sub Main()
+
 #Region "Variablen-Deklaration"
         Dim a As Integer = 5
         Dim b As Integer = 10
@@ -31,22 +32,39 @@
 #End Region
 
 #Region "Select Case"
-        'Select Case Name1
-        '    Case "Hans"
-        '        Console.WriteLine("Hallo, ich bin Hans.")
-        '    Case "Paul"
-        '        Console.WriteLine("Hallo, ich bin Paul")
-        '    Case "Peter"
-        '        Console.WriteLine("Hallo, ich bin Peter")
-        '        ' u.s.w.
-        '    Case Else
-        '        Console.WriteLine("Keiner der geprüften Werte ist in der Variablen enthalten")
-        'End Select
+        Select Case Name1
+            Case "Hans"
+                Console.WriteLine("Hallo, ich bin Hans.")
+            Case "Paul"
+                Console.WriteLine("Hallo, ich bin Paul")
+            Case "Peter"
+                Console.WriteLine("Hallo, ich bin Peter")
+                ' u.s.w.
+            Case Else
+                Console.WriteLine("Keiner der geprüften Werte ist in der Variablen enthalten")
+                Exit Select
+        End Select
 #End Region
 
 #Region "Arrays & Schleifen"
         ' Standard Array-Deklaration mit Längen-Angabe
         Dim array(5) As Integer ' Länge 6
+        Dim array2 = New Integer(,) {{1, 2, 3}, {2, 3, 4}}
+
+        Console.WriteLine($"Länge 1. Dimension: {array2.GetLength(0)}")
+        Console.WriteLine($"Länge 2. Dimension: {array2.GetLength(1)}")
+
+        Console.WriteLine(array2(1, 2))
+
+        'For i = 0 To array2.GetLength(0) - 1
+        '    Console.WriteLine($"Aktuelle Zahl in Dimension 1: {array2(0, i)}")
+        'Next
+
+        'For Each zahl In array2
+        '    Console.WriteLine($"Aktuelle Zahl: {zahl}")
+        'Next
+
+        Console.WriteLine($"UpperBound: {array2.GetUpperBound(1)}")
         'Console.WriteLine($"{array.Length}")
 
         ' Array-Größe erweitern
@@ -122,9 +140,20 @@
 #End Region
 
 #End Region
+
         Console.WriteLine("--- Ende ---")
         Console.ReadLine()
 
     End Sub
+
+#Region "Funktion überladen"
+    Function Addieren(param1 As Integer, param2 As Integer) As Integer
+        Return 0
+    End Function
+
+    Function Addieren(param1 As Double, param2 As Double) As Double
+        Return 0.00
+    End Function
+#End Region
 
 End Module
